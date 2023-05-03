4e2eb76b80322c81d944395214c5e665615e82f4
Ghazi <brahimnewghazi.com>
2023-05-03 04:30:58 +0200

    new changes

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Biblithèque</title>
+</head>
+<body>
+    <header><nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Livres</a></li>
+            <li><a href="#">Catégories</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">S'inscrire</a></li>
+        </ul>
+    </nav>
+    <div class="log">
+        <img src="logo.png" alt="">
+        <h1>Bibliothèque en Ligne</h1>
+    </div>
+    </header>
+
+
+    
+    <form class="search-form">
+    <input type="text" placeholder="Rechercher...">
+    <button type="submit">Rechercher</button>
+</form>
+    <main>
+        <section>
+        <h2>Bienvenue à la Bibliothèque en Ligne</h2>
+        <p>Nous offrons une large sélection de livres dans différents genres, y compris la fiction, la non-fiction, la science-fiction et plus encore.</p>
+        </section>
+        <section>
+        <h2>Derniers livres ajoutés</h2>
+        <ul>
+            <li><a href="#">Code Complete 2: A Practical Handbook of Software Construction by Steve McConnell</a></li>
+            <li><a href="#">Head First Design Patterns</a></li>
+            <li><a href="#">Refactoring</a></li>
+            <li><a href="#">Introduction to Algorithms by Thomas H. Cormen</a></li>
+        </ul>
+        </section>
+        <h2>Derniers livres ajoutés</h2>
+        <ul>
+            <li><a href="#">Artificial Intelligence: A Modern Approach by Stuart Russell</a></li>
+            <li><a href="#">Computer Organization and Design</a></li>
+            <li><a href="#">The C Programming Language by Brian W. Kernighan</a></li>
+            <li><a href="#">Web Development with Node and Express</a></li>
+        </ul>
+        </section>
+        <h2>Derniers livres ajoutés</h2>
+        <ul>
+            <li><a href="#">Algorithms Unlocked by Thomas H. Cormen</a></li>
+            <li><a href="#">Effective Java by Joshua Bloch</a></li>
+            <li><a href="#">Algorithms in C++ by Robert Sedgewick</a></li>
+            <li><a href="#">Effective C++ by Scott Meyers</a></li>
+        </ul>
+        </section>
+    </main>
+    <footer>
+        <p>Tous droits réservés © Bibliothèque en Ligne 2023</p>
+    </footer>
+</body>
+</html>
